ef7559ea2e3add9528363c27accc02942a3a543f
Chris Dearden <christoph4497@gmail.com>
2020-07-23 19:01:52 -0600

    Deprecated Function Change
    TwitchClient.withCredentials is deprecated.
    
    Use TwitchClient constructor with an AuthProvider Object as the authProvider argument.

--- index.js
+++ index.js
@@ -6,38 +6,16 @@ var passport       = require('passport');
 var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 var request        = require('request');
 var handlebars     = require('handlebars');
-var TwitchClient   = require('twitch').default;
-var PubSubClient   = require('twitch-pubsub-client').default;
-var tmi            = require('tmi.js');
-//var botFunc        = require('./botFunctions');
+var TwitchClient = require('twitch').default;
+var PubSubClient = require('twitch-pubsub-client').default;
+var StaticAuthProvider = require('twitch').StaticAuthProvider;
+var RefreshableAuthProvider = require('twitch').RefreshableAuthProvider;
 
 var app = express();
 app.use(session({secret: conf.SESSION_SECRET, resave: false, saveUninitialized: false}));
 app.use(express.static('public'));
 app.use(passport.initialize());
 app.use(passport.session());
-
-console.log(conf.BOT_OAUTH_TOKEN);
-
-// Define configuration options
-const opts = {
-  identity: {
-    username: conf.BOT_USERNAME,
-    password: conf.BOT_OAUTH_TOKEN
-  },
-  channels: [
-    conf.CHANNEL_NAME
-  ]
-};
-
-// Create a client with our options
-const client = new tmi.client(opts);
-
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
-
-client.connect();
 
 OAuth2Strategy.prototype.userProfile = function(accessToken, done) {
     var options = {
@@ -92,20 +70,51 @@ app.get('/auth/twitch', passport.authenticate('twitch', { scope: 'channel:read:r
 app.get('/auth/twitch/callback', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/' }));
 
 var template = handlebars.compile(`
-`);
+<html><head><title>Twitch Auth Sample</title></head>
+<table>
+    <tr><th>Access Token</th><td>{{accessToken}}</td></tr>
+    <tr><th>Refresh Token</th><td>{{refreshToken}}</td></tr>
+    <tr><th>Display Name</th><td>{{display_name}}</td></tr>
+    <tr><th>Bio</th><td>{{bio}}</td></tr>
+    <tr><th>Image</th><td>{{logo}}</td></tr>
+</table></html>`);
 
-app.get('/', function (req, res) {
+app.get('/', async function (req, res) {
     if(req.session && req.session.passport && req.session.passport.user) {
 
-        var accessToken = req.session.passport.user.accessToken;
-        var twitchUserId = req.session.passport.user.data[0].id;
+        const accessToken = req.session.passport.user.accessToken;
+        const refreshToken = req.session.passport.user.refreshToken;
 
-        const twitchClient = TwitchClient.withCredentials(conf.TWITCH_CLIENT_ID, accessToken, 'channel:read:redemptions');
+        //messy but it gets the number correctly
+        const twitchUserId = req.session.passport.user.data[0].id;
+
+        //DEPRECATED --> const twitchClient = TwitchClient.withCredentials(conf.TWITCH_CLIENT_ID, accessToken,'channel:read:redemptions');
+        //use StaticAuthProvider or RefreshableAuthProvider in the TwitchClient Constructor instead.
+        //https://d-fischer.github.io/twitch/reference/classes/ApiClient.html#s_withCredentials
+
+        //create the authprovider
+        const authProvider = new RefreshableAuthProvider(
+          new StaticAuthProvider(conf.TWITCH_CLIENT_ID, accessToken),
+          {
+            secret: conf.TWITCH_SECRET,
+            refreshToken,
+            onRefresh: (token) => {
+              // do things with the new token data, e.g. save them in your database
+            }
+          }
+        );
+
+        //create Client with new AuthProvider
+        const twitchClient = new TwitchClient({authProvider});
         const pubSubClient = new PubSubClient();
-        pubSubClient.registerUserListener(twitchClient, twitchUserId);
-        pubSubClient.onRedemption(twitchUserId, (message) => {
-          onChannelPointReward(message.rewardName);
-        }).then();
+
+        //await waits for a Promise to be fulfilled or rejected.
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
+        await pubSubClient.registerUserListener(twitchClient, twitchUserId);
+
+        const listener = await pubSubClient.onRedemption(twitchUserId, (message) => {
+          console.log(message.rewardName);
+        });
 
         res.send(template(req.session.passport.user));
     } else {
@@ -114,54 +123,5 @@ app.get('/', function (req, res) {
 });
   
 app.listen(3000, function () {
-console.log('Page up on localhost:3000')
+  console.log('Twitch auth sample listening on port 3000!')
 });
-
-function onChannelPointReward(rewardName) {
-  if (rewardName == 'Timeout Joey') {
-    onTimeoutJoey();
-    console.log('Joey Timed Out');
-  } else if (rewardName == 'Timeout Thomas') {
-    onTimeoutThomas();
-    console.log('Thomas Timed Out');
-  }
-}
-
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
-
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-      const num = rollDice();
-      client.say(target, `You rolled a ${num}`);
-      console.log(`* Executed ${commandName} command`);
-  } else {
-      console.log(`* Unknown command ${commandName}`);
-  }
-}
-
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
-
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-}
-
-function onTimeoutJoey() {
-  var timeoutTime = 600;
-  var joeyUsername = 'N7_fishy';
-  client.timeout(conf.CHANNEL_NAME, joeyUsername, timeoutTime, ":)");
-  client.say(conf.CHANNEL_NAME, joeyUsername + " has been timed out for " + timeoutTime + " seconds.")
-}
-
-function onTimeoutThomas() {
-  var timeoutTime = 600;
-  var thomasUsername = 'maligoze';
-  client.timeout(conf.CHANNEL_NAME, thomasUsername, timeoutTime, ":)");
-  client.say(conf.CHANNEL_NAME, thomasUsername + " has been timed out for " + timeoutTime + " seconds.")
-}
